869d63e2d67f7db27ec897b3a3789bb18dee2548
GunnySensei <gunnerwagoner@hotmail.com>
2022-01-31 21:21:30 -0600

    added initial file structure, routes, schema, and CRUD

---
+++ models/User.js
@@ -0,0 +1,63 @@
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
+
+//create User model
+class User extends Model {}
+
+//define table columns and config
+User.init(
+  {
+    //define an id column
+    id: {
+      //use special Sequelize DataTypes object provide what type of data it is
+      type: DataTypes.INTEGER,
+      //same as NOT NULL in sql
+      allowNull: false,
+      //instruct this is Primary Key
+      primaryKey: true,
+      //turn on auto increment
+      autoIncrement: true,
+    },
+    //define username column
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    //define email column
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      //no duplicate email values
+      unique: true,
+      //if allowNull is false, can run data through validation
+      validate: {
+        isEmail: true,
+      },
+    },
+    //define password column
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        //pass must be at least 4 char long
+        len: [4],
+      },
+    },
+  },
+  {
+    //TABLE CONFIG OPTIONS GO HERE (https://sequelize.org/v5/manual/models-definition.html#configuration)
+
+    //pass in imported conection (direct connection to database)
+    sequelize,
+    //don't auto create createdAt/updatedAt timestamp fields
+    timestamps: false,
+    //don't pluralize name of db table
+    freezeTableName: true,
+    //use underscores instead of camel-casing (i.e. `comment_text` and not `commentText`)
+    underscored: true,
+    //make it so our model name stays lowercase in the db
+    modelName: "user",
+  }
+);
+
+module.exports = User;
