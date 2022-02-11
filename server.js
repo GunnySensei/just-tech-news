9a56baf1a88a3eb408336e3a9661419dd08e09f5
GunnySensei <gunnerwagoner@hotmail.com>
2022-02-10 21:57:09 -0600

    testing with Jest implemented for formatting and formatting added to partials

--- server.js
+++ server.js
@@ -3,8 +3,10 @@ const session = require("express-session");
 const routes = require("./controllers");
 const sequelize = require("./config/connection");
 const exphbs = require("express-handlebars");
-const hbs = exphbs.create({});
 const SequelizeStore = require("connect-session-sequelize")(session.Store);
+const helpers = require("./utils/helpers");
+
+const hbs = exphbs.create({ helpers });
 
 const app = express();
 const PORT = process.env.PORT || 3001;
